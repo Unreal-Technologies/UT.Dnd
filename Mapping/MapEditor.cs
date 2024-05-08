24ff77f473ad2a47c744a2b420170906e52fb234
Peter Overeijnder <peterovereijnder@hotmail.com>
2024-05-09 00:36:53 +0200

    -

--- Mapping/MapEditor.cs
+++ Mapping/MapEditor.cs
@@ -1,16 +1,17 @@
-﻿using Shared.Controls;
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Controls;
+using Shared.Efc;
 using Shared.Efc.Tables;
 using Shared.Interfaces;
+using Shared.Modules;
 using System.Drawing;
+using System.Net;
 using System.Windows.Forms;
 using UT.Data.Attributes;
 using UT.Data.Controls;
 using UT.Data.Controls.Custom;
 using UT.Data.Modlet;
-using Shared.Modules;
 using UT.Dnd.Efc.Tables;
-using System.Configuration;
-using System.Net;
 
 namespace UT.Dnd.Mapping
 {
@@ -20,6 +21,7 @@ namespace UT.Dnd.Mapping
         #region Members
         private States state;
         private GridviewGuid? gridviewList;
+        private Guid mapId = Guid.Empty;
         #endregion //Members
 
         #region Constructors
@@ -49,7 +51,7 @@ namespace UT.Dnd.Mapping
 
         public enum Actions
         {
-            CheckIfNameExists
+            CheckIfNameExists, CreateMap
         }
         #endregion //Enums
 
@@ -141,23 +143,106 @@ namespace UT.Dnd.Mapping
                 value is User user
             )
             {
+                Tuple<Guid, string> data = new(user.Id, tabPage_add_vtb_name.Control.Text);
                 Map? map = ModletStream.GetContent<bool, Map>(
                     Client?.Send(
                         ModletStream.CreatePacket(
                             Actions.CheckIfNameExists,
-                            new Tuple<Guid, string>(user.Id, tabPage_add_vtb_name.Control.Text)
+                            data
                         ),
                         ModletCommands.Commands.Action,
                         this
                     )
                 );
+                map ??= ModletStream.GetContent<bool, Map>(
+                    Client?.Send(
+                        ModletStream.CreatePacket(
+                            Actions.CreateMap,
+                            data
+                        ),
+                        ModletCommands.Commands.Action,
+                        this
+                    )
+                );
+                if(map != null)
+                {
+                    State = States.List;
+                }
             }
         }
 
         public override byte[]? OnLocalServerAction(byte[]? stream, IPAddress ip)
         {
+            DbContext? modContext = Context?.Select(this);
+            DbContext? sharedContext = Context == null ? null : Array.Find(Context.List, x => x.GetType() == typeof(SharedModContext));
+            Actions? action = ModletStream.GetInputType<Actions>(stream);
+            if (modContext is not DndModContext dmc || sharedContext is not SharedModContext smc || action == null)
+            {
+                return null;
+            }
 
+            switch (action)
+            {
+                case Actions.CheckIfNameExists:
+                    return OnLocalServerAction_CheckIfNameExists(dmc, stream);
+                case Actions.CreateMap:
+                    return OnLocalServerAction_CreateMap(dmc, smc, stream);
+                default:
+                    break;
+            }
             return null;
+        }
+
+        private static byte[]? OnLocalServerAction_CheckIfNameExists(DndModContext context, byte[]? stream)
+        {
+            Tuple<Guid, string>? tuple = ModletStream.GetContent<Actions, Tuple<Guid, string>>(stream);
+            if (tuple == null)
+            {
+                return null;
+            }
+            Guid userId = tuple.Item1;
+            if(userId == Guid.Empty)
+            {
+                return null;
+            }
+
+            string name = tuple.Item2;
+            Map? map = context.Maps.Where(x => x.User != null && x.User.Id == userId && x.Name == name).FirstOrDefault();
+
+            return ModletStream.CreatePacket(true, map);
+        }
+
+        private static byte[]? OnLocalServerAction_CreateMap(DndModContext dmc, SharedModContext smc, byte[]? stream)
+        {
+            Tuple<Guid, string>? tuple = ModletStream.GetContent<Actions, Tuple<Guid, string>>(stream);
+            if (tuple == null)
+            {
+                return null;
+            }
+            Guid userId = tuple.Item1;
+            if (userId == Guid.Empty)
+            {
+                return null;
+            }
+
+            User? user = smc.Users.FirstOrDefault(x => x.Id == userId);
+            if(user == null)
+            {
+                return null;
+            }
+            dmc.Attach(user);
+
+            string name = tuple.Item2;
+            Map map = new()
+            {
+                Name = name,
+                User = user
+            };
+
+            dmc.Add(map);
+            dmc.SaveChanges();
+
+            return ModletStream.CreatePacket(true, map);
         }
         #endregion //Private Methods
 
